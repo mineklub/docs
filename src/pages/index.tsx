578a6c5cf4352f672d1459887d9bf706ccf34cda
Itz_Hoffe <mhoff770@gmail.com>
2025-01-05 18:04:00 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Docs - Mineclub.dk`}
-      description="Description will go into a meta tag in <head />">
+      description="Mineclub.dk dokumentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
